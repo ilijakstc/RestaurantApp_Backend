53ce8ac03f37952bc60fd2e6c7059e051bf6d8e5
ilijakstc <ilija.kostic@gmx.at>
2025-04-13 14:33:49 +0200

    inital commit

---
+++ src/main/java/personalprojects/restuarantappbackend/RestuarantAppBackendApplication.java
@@ -0,0 +1,13 @@
+package personalprojects.restuarantappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuarantAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestuarantAppBackendApplication.class, args);
+    }
+
+}
